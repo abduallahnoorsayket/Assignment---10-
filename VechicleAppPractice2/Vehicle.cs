e3283d77e8a935b7e0badf45b03c73489cc3ea37
Abdullah noor sayket <abdullahnoorsayket@gmail.com>
2019-10-14 23:10:05 +0600

    my fists commit

---
+++ VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+    class Vehicle
+    {
+        private string vehicleName;
+        private string regNo;
+
+        public double Speed { private get; set; }
+
+        List<double> _speeds = new List<double>();
+
+        //Constructor
+        public Vehicle(string vehicleName, string regNo)
+        {
+            this.vehicleName = vehicleName;
+            this.regNo = regNo;
+
+        }
+
+        public void SetSpeed()
+        {
+            _speeds.Add(this.Speed);
+        }
+        public double GetMinSpeed()
+        {
+            double minSpeed = _speeds.Min();
+            return minSpeed;
+        }
+        public double GetMaxSpeed()
+        {
+            double maxSpeed = _speeds.Max();
+            return maxSpeed;
+        }
+        public double GetAverageSpeed()
+        {
+            double averageSpeed = _speeds.Average();
+            return averageSpeed;
+        }
+    }
+}
---
+++ VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+
+        Vehicle _vehicle;
+        public VehicleUi()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            _vehicle = new Vehicle(vehicleNameTextBox.Text, regNoTextBox.Text);
+
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            _vehicle.Speed = Convert.ToDouble(speedTextBox.Text);
+            _vehicle.SetSpeed();
+
+            speedTextBox.Clear();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = _vehicle.GetMinSpeed().ToString();
+            maxSpeedTextBox.Text = _vehicle.GetMaxSpeed().ToString();
+            averageSpeedTextBox.Text = _vehicle.GetAverageSpeed().ToString();
+        }
+    }
+}
